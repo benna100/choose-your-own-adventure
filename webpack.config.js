d098d4b2e3f74aaf1a527e5bab56f78269ec6945
behuFTP <benjamin.dals.hughes@gmail.com>
2017-11-29 20:22:09 +0100

    Config file update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
-var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
-var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+const path = require('path');
 
 module.exports = {
     entry: {
@@ -9,8 +9,8 @@ module.exports = {
     },
     devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
     output: {
-        path: path.join(__dirname, "dist"),
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
     module: {
         loaders: [{
@@ -19,44 +19,44 @@ module.exports = {
             exclude: /node_modules/,
             query: {
                 plugins: ['transform-runtime'],
-                presets: ['es2015', 'react']
-            }
+                presets: ['es2015', 'react'],
+            },
         }, {
             test: /\.hbs$/,
-            loader: 'handlebars'
+            loader: 'handlebars',
         }, {
             test: /\.css$/,
             loaders: ['style-loader', 'css-loader?importLoaders=1', 'postcss-loader'],
-            exclude: ['node_modules']
+            exclude: ['node_modules'],
         }, {
             test: /\.scss$/,
             loaders: ['style-loader', 'css-loader?importLoaders=1', 'sass-loader'],
-            exclude: ['node_modules']
+            exclude: ['node_modules'],
         }, {
             test: /\.json$/,
-            loader: 'json-loader'
+            loader: 'json-loader',
         }, {
             test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
             exclude: /(node_modules|bower_components)/,
-            loader: "file-loader"
+            loader: 'file-loader',
         }, {
             test: /\.(woff|woff2)$/,
             exclude: /(node_modules|bower_components)/,
-            loader: "url-loader?prefix=font/&limit=5000"
+            loader: 'url-loader?prefix=font/&limit=5000',
         }, {
             test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
             exclude: /(node_modules|bower_components)/,
-            loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+            loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
         }, {
             test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
             exclude: /(node_modules|bower_components)/,
-            loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-        }]
+            loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+        }],
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Intro to Webpack',
-            template: 'index.html'
+            template: './src/index.html',
         }),
     ],
     devtool: 'source-map',
@@ -64,6 +64,6 @@ module.exports = {
         historyApiFallback: true,
         inline: true,
         hot: true,
-        contentBase: './src/'
-    }
+        contentBase: './src/',
+    },
 };
